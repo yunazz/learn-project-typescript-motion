adfdd3aea7c74c8d8c4e6aeceeb5d226ef5997c3
yunazz <67776946+yunazz@users.noreply.github.com>
2021-07-08 20:27:52 +0900

    초기 셋팅

---
+++ src/components/page/page.ts
@@ -0,0 +1,16 @@
+/**
+ * insertAdjacentHTML
+ */
+
+export class PageComponent {
+  private element: HTMLUListElement;
+  constructor() {
+    this.element = document.createElement("ul");
+    this.element.setAttribute("class", "page");
+    this.element.textContent = "This is PageComponent";
+  }
+
+  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
+    parent.insertAdjacentElement(position, this.element);
+  }
+}
