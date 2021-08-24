ac66403c802be2c3ce52d1aa96d4a431fc2484b3
yunazz <67776946+yunazz@users.noreply.github.com>
2021-08-24 17:15:39 +0900

    삭제기능구현

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -3,11 +3,13 @@ import { BaseComponent, Component } from "../component.js";
 export interface Composable {
   addChild(child: Component): void;
 }
+type OnCloseListener = () => void;
 
 class PageItemComponent
   extends BaseComponent<HTMLElement>
   implements Composable
 {
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
               <section class="page-item__body"></section>
@@ -15,12 +17,19 @@ class PageItemComponent
                 <button class="close">&times;</button>
               </div>
           </li>`);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
   addChild(child: Component) {
     const container = this.element.querySelector(
       ".page-item__body"
     )! as HTMLElement;
     child.attachTo(container);
+  }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
 
@@ -36,5 +45,8 @@ export class PageComponent
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
