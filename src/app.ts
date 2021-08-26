18d53154920f7be2e6b87da4c90f2a319f6baaa3
yunazz <67776946+yunazz@users.noreply.github.com>
2021-08-26 13:57:34 +0900

    imageBtn추가

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { InputDialog } from "./components/dailog/dialog.js";
 import { VideoComponent } from "./components/page/item/video.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { NoteComponent } from "./components/page/item/note.js";
@@ -29,6 +30,20 @@ class App {
 
     const todo = new TodoComponent("Todo Title", "Todo Item");
     this.page.addChild(todo);
+
+    const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
+    imageBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+
+      dialog.setOncloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+      dialog.setOnSubmitListener(() => {
+        dialog.removeFrom(document.body);
+      });
+
+      dialog.attachTo(document.body);
+    });
   }
 }
 
